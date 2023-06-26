e9a6ce107c5bf5177e6cd73083a099f9505e3755
Cezary Piątek <cezarypiatek@live.com>
2023-06-26 22:45:34 +0200

    Add full example for sql server

---
+++ src/SmoothSailing.MsSql/MsSqlConfigurationFactory.cs
@@ -0,0 +1,24 @@
+﻿namespace SmoothSailing.MsSql;
+
+public static class MsSqlConfigurationFactory
+{
+    /// <summary>
+    ///     Built on top of helm chart provided by https://github.com/microsoft/mssql-docker/tree/master/linux/sample-helm-chart
+    /// </summary>
+    public static MsSqlConfiguration CreateDefaultConfiguration()
+    {
+        return new MsSqlConfiguration
+        {
+            ImageRepository = "mcr.microsoft.com/mssql/server",
+            ImageTag = "2019-latest",
+            AcceptEulaValue = "Y",
+            MssqlPidValue = "Developer",
+            MssqlAgentEnabledValue = true,
+            Hostname = "mssqllatest",
+            SaPassword = "StrongPass1!",
+            ContainersPortsContainerPort = 1433,
+            ServiceType = "LoadBalancer",
+            ServicePort = 1433
+        };
+    }
+}
