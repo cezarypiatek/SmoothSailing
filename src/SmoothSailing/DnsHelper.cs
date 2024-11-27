5fb0b5b1d996e0b2363454a32dbc9d16fcc12ff6
Cezary Piatek <cezarypiatek@live.com>
2024-11-27 21:10:59 +0100

    Add helper method to resolve working DNS service address

---
+++ src/SmoothSailing/DnsHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SmoothSailing;
+
+internal static class DnsHelper
+{
+    public static async Task WaitForDnsAvailability(string hostName, TimeSpan timeout)
+    {
+        using var cts = new CancellationTokenSource(timeout);
+        try
+        {
+            while (true)
+            {
+                try
+                {
+                    await Dns.GetHostAddressesAsync(hostName);
+                    return;
+                }
+                catch (SocketException)
+                {
+                    await Task.Delay(500, cts.Token);
+                }
+            }
+
+        }
+        catch (OperationCanceledException)
+        {
+            throw new TimeoutException($"The DNS address {hostName} could not be resolved within the timeout period");
+        }
+    }
+}
