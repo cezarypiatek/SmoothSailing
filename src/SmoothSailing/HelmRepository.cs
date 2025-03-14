4ca3803e66cd837a49d5179461254b1c121ad1ab
Cezary Piatek <cezarypiatek@live.com>
2025-03-14 15:17:28 +0100

    Allow to install chart from locally registered repository

--- src/SmoothSailing/HelmRepository.cs
+++ src/SmoothSailing/HelmRepository.cs
@@ -3,13 +3,15 @@
 public class HelmRepository
 {
     public string Url { get; }
-    public string Login { get; }
-    public string Password { get; }
+    public string? Login { get; }
+    public string? Password { get; }
 
-    public HelmRepository(string url, string login, string password)
+    public HelmRepository(string url, string? login, string? password)
     {
         Url = url;
         Login = login;
         Password = password;
     }
+    
+    public static HelmRepository LocallyAvailable => new HelmRepository(string.Empty, null, null);
 }
